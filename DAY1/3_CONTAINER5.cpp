d9041984399cf2977af2fb9a3d19f433c9171a9c
ecourse <smkang@codenuri.co.kr>
2023-08-07 12:11:23 +0900

    Add files via upload

--- DAY1/3_CONTAINER5.cpp
+++ DAY1/3_CONTAINER5.cpp
@@ -18,17 +18,29 @@ int main()
 
 	// 뒤집는 알고리즘은 std::reverse 입니다.
 	// v의 모든 요소 뒤집어 보세요
-	// ?
+	std::reverse(v.begin(), v.end());
 
 	std::list<int> s = { 1,2,3,4,5 };
 	// s도 뒤집어 보세요
-	// ?
+	std::reverse(s.begin(), s.end());
 	
 	for (auto e : v) { std::cout << e << std::endl; }
 	for (auto e : s) { std::cout << e << std::endl; }
 }
 
+// std::reverse(v) 로 하면 더 간단하지 않나요 ?? 
+// => C++98 당시 구현에 어려움이 있었습니다. 이유는 내일 소개
+// => C++20 부터 됩니다. - concept 이라는 새로운 문법으로 해결
+// std::ranges::reverse(v); // ok
 
+// cppreference.com 접속 해 보세요
+//-------------------------------
+// boost.org 에 접속 해보세요
 
+// STL : 모든 것이 엄격하게 검증한 후에 추가됨 C++ 표준
 
+// boost : 1999년 발표된 C++ 범용적 라이브러리
+//         1년에도 3~4회씩 계속 update 됨
+//		   여기서 검증이 끝나면 대부분 표준에 추가됨.
 
+// STL : Standard Template Library
