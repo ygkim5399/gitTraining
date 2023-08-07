fbe7e18a18fb403bb196d383c7dee99a413e42ad
ecourse <smkang@codenuri.co.kr>
2023-08-07 09:09:38 +0900

    Add files via upload

--- DAY1/1_STL_PREVIEW1.cpp
+++ DAY1/1_STL_PREVIEW1.cpp
@@ -4,13 +4,35 @@
 
 int main()
 {
+	// C언어 에서의 문자열 처리 : char 배열 또는 포인터 사용
 	char s1[] = "Hello";
 	char s2[] = "Hello";
 
-	if (s1 == s2) {}
-	
-//	s2 = s1;
+//	if (s1 == s2) {} // 문자열의 상등을 비교하려는 의도 인데..
+					 // 실제로는 배열의 주소 비교
+					 // 항상 false 
+	if ( strcmp(s1, s2) == 0 ) {} //  ok
+
+//	s2 = s1; // 의도 : 문자열 대입.
+			 // 하지만 error. 배열의 이름에 대입할수 없다.
+//	strcpy(s2, s1);
+//	strcpy_s(s2, strlen(s1), s1);
+
+	// C++ 은 std::string 을 사용하면 편리합니다.
+	// => 문자열을 정수형 변수처럼 연산 가능
+	std::string s3 = "Hello";
+	std::string s4 = "Hello";
+
+	if (s3 == s4) {}
+	s3 = s4;
+	s3 = s4 + s4;
 }
+
+
+
+
+
+
 
 // DAY1.zip 압축 풀러 보세요
 
