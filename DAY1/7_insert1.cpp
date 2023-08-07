5e987d2a86f8e971907e8efdac13b828c43511e7
ecourse <smkang@codenuri.co.kr>
2023-08-07 16:54:12 +0900

    Add files via upload

---
+++ DAY1/7_insert1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+
+int main()
+{
+	std::list<int> s = { 1, 2, 3, 4, 5};
+
+	// 컨테이너에 요소를 추가하려면
+	// 방법 1. 멤버 함수 push_back() 사용
+	s.push_back(10);
+
+
+	// 방법 2. 삽입 반복자 사용
+	std::back_insert_iterator bi(s);
+
+	*bi = 20;
+	*bi = 30;
+}
