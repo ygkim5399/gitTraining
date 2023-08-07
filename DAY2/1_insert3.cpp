b89380b04a61811ad39ee9a43c6aee3d6fde7243
ecourse <smkang@codenuri.co.kr>
2023-08-08 08:44:32 +0900

    Add files via upload

--- DAY2/1_insert3.cpp
+++ DAY2/1_insert3.cpp
@@ -13,6 +13,7 @@ int main()
 	// 아래 2줄 차이점 잘 알아 두세요
 	std::copy(v.begin(), v.end(), s1.begin()); 
 							// v 의 모든 요소를 s1에 덮어쓴것
+
 	std::copy(v.begin(), v.end(), std::back_inserter(s2));
 							// v 의 모든 요소를 s2에 끝에 추가
 
