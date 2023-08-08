4bf5af4ed3f2d4e0ba8852f3b8b94c6ba772be4f
ecourse <smkang@codenuri.co.kr>
2023-08-08 15:46:32 +0900

    Add files via upload

--- DAY2/6_함수객체3.cpp
+++ DAY2/6_함수객체3.cpp
@@ -5,14 +5,25 @@
 
 bool foo(int n) { return n % 3 == 0; }
 
+struct IsDivide
+{
+	int value;
+
+	IsDivide(int n) : value(n) {}
+
+	bool operator()(int n) { return n % value == 0; }
+};
+
 int main()
 {
 	std::vector<int> v = { 1,2,6,4,5,3,7,8,9,10 };
 
-	// 주어진 구간에서 "3의 배수" 를 찾고 싶다.
-	auto p1 = std::find_if(v.begin(), v.end(), foo );
+	// 주어진 구간에서 "k의 배수" 를 찾고 싶다.
+	int k = 3;
 
+	IsDivide f(k); // f는 내부적으로 k값을 보관하게 됩니다.
 
+	auto p1 = std::find_if(v.begin(), v.end(), f );
 }
 
 
