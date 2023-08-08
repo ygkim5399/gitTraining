95e1900819b4e9a73533dbd6929487b3eb3b809c
ecourse <smkang@codenuri.co.kr>
2023-08-08 09:42:43 +0900

    Add files via upload

--- DAY2/2_stream3.cpp
+++ DAY2/2_stream3.cpp
@@ -8,12 +8,15 @@ int main()
 	std::ostream_iterator<char> p1(std::cout, "");
 
 
+
 	std::ifstream fin("2_stream3.cpp"); // 입력 파일
 
 	std::istream_iterator<char> p2(fin); // p2는 fin 파일에서
 										// char 단위로 읽는 반복자
 
 	std::cout << *p2 << std::endl; // #
+	+ 
+	std::cout << *p2 << std::endl; // i
 
 
 }
