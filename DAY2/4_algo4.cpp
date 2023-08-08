2a6bbb156bd421f6c01c3ac81c1b8303ca426cb1
ecourse <smkang@codenuri.co.kr>
2023-08-08 12:02:03 +0900

    Add files via upload

---
+++ DAY2/4_algo4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+	std::list<int>   s = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// v와 s를 각각 뒤집어(reverse) 해 보세요
+
+
+	show(v);
+	show(s);
+}
+
