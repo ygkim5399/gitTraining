2631406344ca816dc561e2be59a96556e8700a12
ecourse <smkang@codenuri.co.kr>
2023-08-08 13:58:52 +0900

    Add files via upload

--- DAY2/6_알고리즘과함수4.cpp
+++ DAY2/6_알고리즘과함수4.cpp
@@ -14,6 +14,13 @@ int main()
 	auto p2 = std::find_if(v.begin(), v.end(), [](int n) { return n % 3 == 0; });
 
 
+	int k = 3; // 사용자가 입력한 값이라고 가정..
+
+	// 주어진 구간에서 "k의 배수"를 찾고 싶다.
+	// => foo 함수를 어떻게 만들어야 할까요 ?
+	auto p3 = std::find_if(v.begin(), v.end(), foo);
+	auto p4 = std::find_if(v.begin(), v.end(), [](int n) { return n % 3 == 0; });
+
 }
 
 
