d8434540e8fc2a7ed113aca07b465fb8fbc07fbb
ecourse <smkang@codenuri.co.kr>
2023-08-08 13:41:34 +0900

    Add files via upload

--- DAY2/5_알고리즘과함수2.cpp
+++ DAY2/5_알고리즘과함수2.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include "show.h"
 
+// 이항 함수
+int foo(int a, int b) { return a + b; }
 
 int main()
 {
@@ -13,8 +15,10 @@ int main()
     // transform 알고리즘
     std::transform( s1.begin(), s1.end(),  // 1. 이 구간의 모든 요소를
                     s2.begin(),            // 2. 이 구간의 요소와 함께
-                    s3.begin(),            // 4. 3의 결과를 이곳에 넣어달라
-                    foo);                  // 3. 이함수에 보내서 
+                    std::back_inserter(s3),// 4. 이곳에 넣어달라
+                    foo);                  // 3. 이함수에 보낸후 반환값을
+
+    show(s3);
 
 	return 0;
 }
