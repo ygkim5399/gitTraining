4c65e98b8d0287d4248a2323a57804440f4346ae
ecourse <smkang@codenuri.co.kr>
2023-08-08 10:06:58 +0900

    Add files via upload

---
+++ DAY2/3_adapter1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10};
+
+	auto p1 = s.begin();
+	auto p2 = std::next(p1, 5);
+
+
+	// reverse_iterator : ++ 연산시 "거꾸로 동작" 하는 반복자
+	// std::reverse_iterator< 반복자타입 > p3( 반복자 );
+
+	std::reverse_iterator< std::list<int>::iterator > p3(p2); // p3는  "p2-1" 위치로 초기화 됩니다.
+
+
+	std::cout << *p3 << std::endl; // 5
+	++p3;
+	std::cout << *p3 << std::endl; // 4
+}
