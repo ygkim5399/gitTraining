580263747cbf75366c8447751ed0414ceba51e40
ecourse <smkang@codenuri.co.kr>
2023-08-08 13:49:40 +0900

    Add files via upload

---
+++ DAY2/6_알고리즘과함수3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,4,5,3,7,8,9,10 };
+
+	// std::find    : 값 검색
+	// std::find_if : 조건 검색
+
+	// 주어진 구간에서 3을 찾고 싶다.
+	auto p1 = std::find(v.begin(), v.end(), 3);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
