b8b0f4469a7261a8aab68468ae13f4d091a9a47a
ecourse <smkang@codenuri.co.kr>
2023-08-08 09:55:23 +0900

    Add files via upload

--- DAY2/2_stream5.cpp
+++ DAY2/2_stream5.cpp
@@ -13,6 +13,17 @@ void show(const C& c)
 	std::cout << "\n";
 }
 
+// 2. iterator 버전
+template<typename T>
+void show(T first, T last)
+{
+	std::ostream_iterator< ? > p(std::cout, ", ");
+
+	std::copy(first, last, p);
+
+	std::cout << "\n";
+}
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
@@ -22,4 +33,9 @@ int main()
 	// 아래 함수를 만들어 봅시다.
 	show(v);
 	show(x);
+
+	show(x, x + 5);
+	show(v.begin(), std::next(v.begin(), 5));
+					// "v.begin() + 5" 도 가능(임의접근 이므로)
+					// 하지만 v가 list 라면 std::next() 필요
 }
