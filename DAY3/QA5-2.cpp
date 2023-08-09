3c682ecd0f31082b59add2919018c87baf437626
ecourse <smkang@codenuri.co.kr>
2023-08-09 16:00:26 +0900

    Add files via upload

--- DAY3/QA5-2.cpp
+++ DAY3/QA5-2.cpp
@@ -8,9 +8,10 @@ int main()
 	std::string s2 = "B";
 
 	// 아래 4줄을 생각해 보세요
-	auto a1 = std::max(1,  2);
-	auto a2 = std::max(s1, s2);
-	auto a3 = max(1,  2);
-	auto a4 = max(s1, s2);
+	auto a1 = std::max(1,  2);	// ok.  max 는 std:: 안에 있습니다.
+	auto a2 = std::max(s1, s2); // ok.
+
+	auto a3 = max(1,  2);	// error. 
+	auto a4 = max(s1, s2);	// ok.  s1, s2 가 std:: 안에 있으므로 자동검색
 
 }
