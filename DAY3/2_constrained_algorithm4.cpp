c99cd967711445be530b68b6cea13b4f5b30bc3e
ecourse <smkang@codenuri.co.kr>
2023-08-09 09:20:07 +0900

    Add files via upload

--- DAY3/2_constrained_algorithm4.cpp
+++ DAY3/2_constrained_algorithm4.cpp
@@ -9,4 +9,9 @@ int main()
 	int n1 = std::max(1, 2);		// C++98 알고리즘은 "함수(템플릿)" 입니다
 	int n2 = std::ranges::max(1, 2);// C++20 알고리즘은 "함수 객체" 입니다.
 	int n3 = std::ranges::max.operator()(1, 2);
+
+	// C++20 에서 추가된 알고리즘들을
+	// "constrained algorithm" 이라고도 부릅니다.
+
+	// 보통은 "ranges algorithm" 이라고 부릅니다.
 }
