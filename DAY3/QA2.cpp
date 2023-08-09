47e0b2e44221bea6bca8a5dba5688eea21cabb20
ecourse <smkang@codenuri.co.kr>
2023-08-09 11:41:57 +0900

    Add files via upload

--- DAY3/QA2.cpp
+++ DAY3/QA2.cpp
@@ -4,7 +4,7 @@
 int foo()
 {
 	int n = 3;
-	return n;
+	return n; // n의 값 3 반환
 }
 
 void set(int& n, int v)
@@ -16,14 +16,27 @@ int main()
 {
 	int x = 0;
 	set(x, 10); // ok..   x = 10,   x 는 lvalue
-	set(3, 10); // error. x = 10,   3 는 rvalue
+
+	set(3, 10); // error. 3 = 10,   3 는 rvalue
+	
 	set(foo(), 10); // error.  foo 의 반환값은 n 이 아닌 3
+					//		3 = 10
 
 	// 핵심 : int& 는 int 타입의 lvalue 만 받을수 있다.
 
 	std::vector<int> v = { 1,2,3,4,5 };
 	auto it = v.begin();
-	std::advance(it, 3);
+
+	// advance 모양 : advance(T& it, int n)
+	// => 따라서 advance 의 1번째 인자는 lvalue(이름있는 변수) 만 가능
+	// => v.begin() 은 반복자를 임시객체(rvalue)로 반환
+	std::advance(it, 3);  
 	std::advance(v.begin(), 3);
 
+	// => lvalue, rvalue 개념이 필요 하고
+	// => T& 는 임의 타입의 lvalue 만 받을수 있다는 문법을 알아야 합니다.
+	// => 임시객체 개념을 알아야 하고, 임시객체를 rvalue 라는 것도 필요
+
+	std::advance(p, 3); // p자체를 이동
+	std::next(p, 3);    // p자체는 변하지 않고, 새로운 반복자를 반환
 }
