1d14db3f9a03674c21fd0b711a16f5e702592bf8
ecourse <smkang@codenuri.co.kr>
2023-08-09 15:40:04 +0900

    Add files via upload

---
+++ DAY3/QA4.cpp
@@ -0,0 +1,12 @@
+// QA4
+#include <iostream>
+#include <algorithm>
+using namespace std; // 왜?? 이코드를 사용 안하세요 ??
+					 // => 실전에서는 되도록 std 열어 놓지 마세요
+
+int count = 10;
+
+int main()
+{
+	int n = count; // ??? 왜 에러일까요 ?? 원인 생각해 보세요
+}
