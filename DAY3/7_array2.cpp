d8052b8037ba60eb9e1ecbf267c5218967acdd12
ecourse <smkang@codenuri.co.kr>
2023-08-09 14:41:57 +0900

    Add files via upload

--- DAY3/7_array2.cpp
+++ DAY3/7_array2.cpp
@@ -3,13 +3,19 @@
 #include <array>
 
 // 아래 코드를 생각해 보세요
-struct array
+template<typename T, std::size_t N> struct array
 {
-	int buff[5];
+	T buff[N];
+
+	int size() const { return N; }
 };
 
 int main()
 {
 	// 구조체 안에 배열이 있으면 아래 처럼 초기화 가능합니다.
-	array arr = { 1,2,3,4,5 };
+	std::array<int, 5> arr = { 1,2,3,4,5 };
+
+	int sz = arr.size();
+	
+	arr.resize(10); // 될까요 ??
 }
