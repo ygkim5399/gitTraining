b9bc909537606981d13c7a3bb9c3b5e977e535d2
ecourse <smkang@codenuri.co.kr>
2023-08-09 15:48:36 +0900

    Add files via upload

--- DAY3/QA5.cpp
+++ DAY3/QA5.cpp
@@ -5,7 +5,6 @@ namespace Graphic
 	{
 		int x, y;
 	};
-
 	void init(const Point& pt) {}
 }
 
@@ -14,6 +13,8 @@ int main()
 //	Point pt; // error.
 	Graphic::Point pt; // ok
 
-	init(pt); // ???? 왜 에러가 아닐까요 ??
-
+	init(pt);	// ok..
+				// 인자 pt 가 Graphics 이름 공간 안에 있으므로
+				// init 을 찾을때 자동으로 Graphics 이름 공간도 검색에 포함
+				// "인자 기반 탐색( Argument Dependent Lookup, ADL)"
 }
