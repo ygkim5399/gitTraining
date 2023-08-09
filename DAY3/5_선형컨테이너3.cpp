2f81d7f2b1ffe6fe222d878b2bd3cb7afca93940
ecourse <smkang@codenuri.co.kr>
2023-08-09 11:52:19 +0900

    Add files via upload

--- DAY3/5_선형컨테이너3.cpp
+++ DAY3/5_선형컨테이너3.cpp
@@ -12,10 +12,22 @@ int main()
 					// => 기존 메모리 계속 사용하고
 					// => size 변수만 변경합니다.
 
-	std::cout << v.size() << std::endl;		// 7
+	std::cout << v.size()     << std::endl;	// 7
 	std::cout << v.capacity() << std::endl;	// 10
-					
 
+	v.push_back(5); // 끝에 한개(5) 추가
+					// capacity > size 이므로
+					// 이 연산은 메모리 재할당이 필요 없습니다
+					// 아주 빠르게 동작합니다.
+
+	std::cout << v.size() << std::endl;	    // 8
+	std::cout << v.capacity() << std::endl;	// 10					
+
+	v.shrink_to_fit(); // size == capacity 로 해달라
+					   // 결국 이순간 8개 크기 메모리 재할당해서 사용
+
+	std::cout << v.size() << std::endl;	    // 8
+	std::cout << v.capacity() << std::endl;	// 8	
 	
 }
 
