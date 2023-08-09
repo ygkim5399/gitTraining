16c5feda1d380c29b29582085755279a7a7f97ee
ecourse <smkang@codenuri.co.kr>
2023-08-09 17:10:13 +0900

    Add files via upload

--- DAY3/9_유틸리티5.cpp
+++ DAY3/9_유틸리티5.cpp
@@ -11,9 +11,12 @@ int main()
 
 	std::istringstream iss("to be or not to be");
 
+
 	std::string s;
 
-	iss >> s; // iss 에서 한단어 입력
+	while(iss >> s) // iss 에서 한단어 입력
+		std::cout << s << std::endl;
 
-	std::cout << s << std::endl;
+	// 결국 위 코드는 "한개의 문장에서 단어별로 분리해서 꺼내고 있는것"
+	// => C언어의 strtok() 기능을 수행하고 있는것
 }
