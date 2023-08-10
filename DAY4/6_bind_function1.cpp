041774f50df3b335c6ecece141c9698731618396
ecourse <smkang@codenuri.co.kr>
2023-08-10 13:44:15 +0900

    Add files via upload

--- DAY4/6_bind_function1.cpp
+++ DAY4/6_bind_function1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <functional>
+using namespace std::placeholders; // _1, _2, _3 ....
 
 // 4항 함수
 void foo( int a, int b, int c, int d)
@@ -15,6 +16,11 @@ int main()
     // std::bind : M항 함수의 일부 인자를 고정해서
     //             N항 함수를 반환. (단, M > N )
 
-    std::bind(&foo, 3, 4, 1, 2);
+    // 사용법 : std::bind( M항함수, M개 인자 )
+
+    std::bind(&foo, 3, 4, 1, 2)(); // 결국, foo(3, 4, 1, 2)의미
+    //|<== bind 반환값이 함수=>|    // 4항함수 => 0항 함수로 변경한것
+
+    std::bind(&foo, 8, _1, 6, _2)(3, 7);  
 }
 
