6feb126ba015fc70d5ad401685fb51c8699ac971
ecourse <smkang@codenuri.co.kr>
2023-08-10 14:40:14 +0900

    Add files via upload

--- DAY4/6_bind_function4.cpp
+++ DAY4/6_bind_function4.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <algorithm>
-#include <functional>
+#include <functional> // std::bind, std::function
 #include <vector>
+using namespace std::placeholders;
 
+bool foo(int a, int b)
+{
+	return a % b == 0;
+}
 
 int main()
 {
@@ -11,7 +16,13 @@ int main()
 	int k = 3;
 
 	// 주어진 구간에서 k이 배수를 찾고 싶다.
-	auto ret = std::find_if(v.begin(), v.end(), ? );
+//	auto ret = std::find_if(v.begin(), v.end(), foo ); // error
+					// => find_if 는 3번째 인자로 단항함수를 요구
+					// => 그런데, foo 는 2항 함수
+					
+	auto ret = std::find_if(v.begin(), v.end(), std::bind(&foo, _1, k)   );
+
+	std::cout << *ret << std::endl;
 }
 
 
