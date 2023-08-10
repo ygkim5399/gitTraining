fffc227f1d23964d680db5bb38f71011108dd6a6
ecourse <smkang@codenuri.co.kr>
2023-08-10 10:09:30 +0900

    Add files via upload

--- DAY4/2_map2.cpp
+++ DAY4/2_map2.cpp
@@ -12,12 +12,29 @@
 
 int main()
 {
-	std::ifstream f("2_map2.cpp"); 
+	std::ifstream f("2_map2.cpp");
 
 	int no = 0;
 	std::string s;
+	std::string word;
 
-	std::map<std::string, std::list<int>> index_map; // 핵심.. 구조 생각해보세요
+	std::map< std::string, std::list<int> > index_map; // 핵심.. 구조 생각해보세요
+
+	while (std::getline(f, s)) // 파일에서 한줄 읽기
+	{
+		++no; // line number 증가
+
+		std::istringstream iss(s);
+
+		while (iss >> word)	// 한줄(문장)을 단어로 분리
+		{
+			// [] 연산 : word 가 이미 있으면 value(list) 반환
+			//			 없으면 새롭게 생성
+			index_map[word].push_back(no);
+		}
+	}
 }
+
+
 
 
