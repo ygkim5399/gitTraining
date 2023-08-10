d0db70006687b05ded4333ac085cf4a8d3db857d
ecourse <smkang@codenuri.co.kr>
2023-08-10 14:36:46 +0900

    Add files via upload

---
+++ DAY4/6_bind_function4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,5,4 };
+
+	int k = 3;
+
+	// 주어진 구간에서 k이 배수를 찾고 싶다.
+	auto ret = std::find_if(v.begin(), v.end(), ? );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+//
