5e559edd94d6357d4c8ef3b13810f00aeae112bb
ecourse <smkang@codenuri.co.kr>
2023-08-11 11:34:27 +0900

    Add files via upload

---
+++ DAY5/4_thread1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread> 
+#include <chrono>
+using namespace std::literals; // 이 안에 s, ns, us, min 등의
+								// 리터럴 접미사 제공			
+// this_thread namespace 
+// => 스레드 관련 4개 함수 제공
+
+int main()
+{
+	std::cout << std::this_thread::get_id() << std::endl;
+
+	std::this_thread::sleep_for(3s);
+
+	std::this_thread::sleep_until(시간); // 시간 "까지" 재우기
+	std::this_thread::yield();		// 현재 스레드 실행흐름을
+									// 중단하고, 다른 스레드 실행
+}
+
+
